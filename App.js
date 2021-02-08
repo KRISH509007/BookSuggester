1b5b50e4fa8f723ef857fc575b058ffc1dbd1f46
KRISH509007 <email4gokul@gmail.com>
2021-02-08 15:15:56 +0530

    done

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer ,createSwitchNavigator } from 'react-navigation';
+import SuggestionScreen from './screens/SuggestionScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+  export default class App extends React.Component {
+     render(){
+       return ( <AppContainer/> ); } }
+ 
+
+const switchNavigator = createSwitchNavigator(
+  { LoginScreen:
+    {screen: LoginScreen}, TabNavigator:
+    {screen: TabNavigator} }) 
+  const AppContainer = createAppContainer(switchNavigator);
+
+
+
 
 const styles = StyleSheet.create({
   container: {
